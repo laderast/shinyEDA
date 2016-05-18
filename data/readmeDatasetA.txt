86279ccbb30dc5893a70257193a49d2042e1d679
laderast <roshioshi@gmail.com>
2016-05-18 16:31:54 -0700

    Second commit, new Data and data dicts

---
+++ data/readmeDatasetA.txt
@@ -0,0 +1,6 @@
+Readme for Dataset A.
+
+This data consists of volunteer subjects tested at a local university over a two month period. Patients were randomly assigned to have either a treatment (an experimental weight loss drug) or placebo. Since the study was double-blinded, neither the nurses who conducted the measurements nor the patients knew whether they received placebo or not.
+
+Please refer to dataDictionaryDatasetA for a description of each field.
+
--- global.R
+++ global.R
@@ -2,18 +2,20 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-dataset <- read.delim("data/datasetA.txt")
+dataset <- read.delim("data/datasetA.txt",row.names = 1)
 
 #  dataset <- dataset %>% filter(startWeights != 9999 & endWeights != 9999)
 #  dataset <- dataset %>% mutate(weightLoss = startWeights - endWeights, percentWeight = weightLoss / timeElapsed)
 #  dataset <- na.omit(dataset)
 
-dataset <- read.csv("data/datasetB.csv")
+#dataset <- read.csv("data/datasetB.csv")
 #dataset <- dataset %>% filter(nurseID != "Nurse3")
 #dataset <- na.omit(dataset)
 #dataset <- dataset %>% mutate(startWeight2 = startWeight * 2.2, 
 #                              endWeight2 = endWeight * 2.2, weightLoss = startWeight2 - endWeight2)
 
+
+##Don't modify anything below here, or app won't work properly.
 
 #get the variable types
 varClass <- sapply(dataset, class)
