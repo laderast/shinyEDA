7d59b3c2e72dbd3e1574e8e75156b04ab685fa0b
laderast <roshioshi@gmail.com>
2016-05-25 15:31:16 -0700

    updated answers.R

--- answers/answers.R
+++ answers/answers.R
@@ -1,6 +1,8 @@
 #Code to process dataset A
 library(dplyr)
 
+#load dataset A
+
 datasetA <- read.delim("data/datasetA.txt",row.names = 1)
 datasetA <- datasetA %>% filter(startWeights != 9999 & endWeights != 9999)
 datasetA <- datasetA %>% mutate(weightLoss = startWeights - endWeights, weightLossPerDay = weightLoss / timeElapsed)
@@ -13,12 +15,21 @@ datasetB <- read.csv("data/datasetB.csv", row.names = 1)
 datasetB <- datasetB %>% filter(staffID1 != "S3")
 datasetB <- na.omit(datasetB)
 datasetB <- datasetB %>% mutate(weightLoss = (startWeight - endWeight) * 2.2, 
-                               weightLossPerDay = weightLoss / timeElapsed)
+                                weightLossPerDay = weightLoss / timeElapsed)
 dataset <- datasetB
 
 #Code to combine the two datasets
-#Here we select only two columns of each dataset (treatment and weight)
-datasetAselect <- datasetA %>% mutate(site="A", gender=tolower(gender)) %>% select(treatment, weightLossPerDay, site, gender)
-datasetBselect <- datasetB %>% mutate(site="B", gender=tolower(gender)) %>% select(treatment, weightLossPerDay, site, gender)
+#Here we select columns for each dataset (treatment and weight)
+#in order to make the two datasets comparable, we make both genders lowercase 
+#(coverting it to a character), but then we have to recast it as a 
+#factor (categorical variable)
+datasetAselect <- datasetA %>% mutate(site="A", gender=factor(tolower(gender))) %>% 
+  select(treatment, weightLossPerDay, site, gender)
+
+datasetBselect <- datasetB %>% mutate(site="B", gender=factor(tolower(gender))) %>% 
+  select(treatment, weightLossPerDay, site, gender)
 
 dataset <- rbind(datasetAselect, datasetBselect)
+
+#have to recast site as a factor because it's a character
+dataset$site <- factor(dataset$site)
